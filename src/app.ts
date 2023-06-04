0e96547e36c3ee6b4db2a89ffeb60f3afdfdce13
victoria123ar <vic_andrade_@hotmail.com>
2023-06-04 19:31:22 -0300

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
